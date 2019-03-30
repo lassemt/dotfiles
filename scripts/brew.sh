0e2293193d05d15390d51a12aa113381fef50535
Lasse Mejlvang Tvedt <himself@lassemt.com>
2019-03-30 15:05:16 +0100

    Create brew.sh

---
+++ scripts/brew.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim
+brew install grep
+brew install openssh
+brew install screen
+
+
+# Basics
+brew install node
+brew install git
+brew install git-lfs
+brew install zsh
+
+# set zsh as your default shell
+sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
+
+# Tools
+brew install tree                             # List dirs as tree graphs
+brew install wget                             # Download stuff from the web
+brew install youtube-dl                       # Download videos
+brew install prettyping                       # Nicer ping command
+brew install python3                          # Python
+brew install composer                         # PHP Package manager
+brew install zsh-autosuggestions              # ZSH autosuggestions
+brew install zsh-completions                  # ZSH compleytes
+brew install zsh-syntax-highlighting          # ZSH syntax highlightning
+
+brew tap caskroom/cask
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json webpquicklook suspicious-package quicklookase qlvideo 
+
+# GPG Tools
+# brew cask install gpg-suite
