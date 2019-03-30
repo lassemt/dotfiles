40ac7aed8b7437a89003228d2904d3bfb51af040
Lasse Mejlvang Tvedt <himself@lassemt.com>
2019-03-31 00:16:31 +0100

    Update npm.sh

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+npmdir="${HOME}/.npm-packages"
+
+# Create if folder dont exists
+if [ ! -d ${npmdir} ]; then
+    mkdir -p ${npmdir}
+    npm config set prefix $npmdir
+fi
+
+# Set permissions for global modules folder
+me=`whoami`
+sudo chown -R $me $npmdir
+
+# Install modules
 npm install -g n  # node verison manager
 npm install -g npm-check-updates  # upgrades package.json dependencies
 npm install -g pure-prompt # Pretty, minimal and fast ZSH prompt
