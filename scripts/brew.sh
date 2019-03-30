91e22dcf304f9bc938848ea4075106e30fa4e01c
Lasse Mejlvang Tvedt <himself@lassemt.com>
2019-03-30 15:33:09 +0100

    Update brew.sh

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -38,5 +38,22 @@ brew tap caskroom/cask
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json webpquicklook suspicious-package quicklookase qlvideo 
 
+# Browsers
+brew cask install google-chrome
+brew cask install firefox
+
+# Other software
+brew cask install 1password
+brew cask install docker
+brew cask install sublime-text
+brew cask install iterm2
+brew cask install dropbox
+brew cask install adobe-creative-cloud
+brew cask install suitcase-fusion
+brew cask install discord
+brew cask install sketch
+brew cask install spotify
+brew cask install zeplin
+
 # GPG Tools
 # brew cask install gpg-suite
